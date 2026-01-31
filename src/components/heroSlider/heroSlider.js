1df4abf62e4d5aec5c168af0b808ecbb1cbba62c
mizza-cz <mizza.cz666@gmail.com>
2026-01-31 07:32:39 +0100

    add hp

---
+++ src/components/heroSlider/heroSlider.js
@@ -0,0 +1,16 @@
+const sliderHero = document.querySelector(".heroSwiper");
+if (sliderHero) {
+  new Swiper(".heroSwiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+}
