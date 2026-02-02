4d6c3c1d743fedb2fa7385fa96d13fef50184241
mizza-cz <mizza.cz666@gmail.com>
2026-02-02 12:03:00 +0100

    fix slider hero

--- src/components/heroSlider/heroSlider.js
+++ src/components/heroSlider/heroSlider.js
@@ -1,15 +1,26 @@
 const sliderHero = document.querySelector(".heroSwiper");
+
 if (sliderHero) {
   new Swiper(".heroSwiper", {
     cssMode: true,
+
+    loop: true, // 👈 инфинити
+
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
+
     mousewheel: true,
     keyboard: true,
   });
